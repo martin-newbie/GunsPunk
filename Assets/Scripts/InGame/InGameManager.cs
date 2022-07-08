c54b63ca39adacd2d2f168850ee2fc9ff1d88f55
martin_newbie <g.martin.min@gmail.com>
2022-07-09 00:33:10 +0900

    d

--- Assets/Scripts/InGame/InGameManager.cs
+++ Assets/Scripts/InGame/InGameManager.cs
@@ -52,7 +52,7 @@ public class InGameManager : Singleton<InGameManager>
     {
         while (true)
         {
-            yield return new WaitForSeconds(hurdleSpawnSpeed);
+            yield return new WaitForSeconds(hurdleSpawnSpeed + Random.Range(-3f, 1f));
             SpawnHurdle();
         }
     }
