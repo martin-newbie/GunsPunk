06fabae2a99f5608451e57e09e7c71389a1f8a7e
martin_newbie <g.martin.min@gmail.com>
2022-07-16 10:01:08 +0900

    add bullet shell effect

--- Assets/Scripts/InGame/Objects/GroundScroll.cs
+++ Assets/Scripts/InGame/Objects/GroundScroll.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteScroll : MonoBehaviour
+public class GroundScroll : MonoBehaviour
 {
     MeshRenderer SR;
     float offset;
--- Assets/Scripts/InGame/PlayerBase.cs
+++ Assets/Scripts/InGame/PlayerBase.cs
@@ -30,6 +30,7 @@ public abstract class PlayerBase : JumpAble
     public float HealthIncrease;
 
     [Header("Objects")]
+    public ParticleSystem BulletShellEffect;
     public Transform FirePos;
     public Bullet bullet;
 
@@ -130,7 +131,7 @@ public abstract class PlayerBase : JumpAble
             _bullet = Instantiate(bullet, FirePos.position + new Vector3(0, Random.Range(-spread_pos, spread_pos)), Quaternion.Euler(0, 0, Random.Range(-spread_rot, spread_rot)));
             _bullet.Init(speed, damage, this);
             AmmoCount--;
-
+            BulletShellEffect.Play();
             InGameManager.Instance.SetRoundCoin(1);
         }
 
