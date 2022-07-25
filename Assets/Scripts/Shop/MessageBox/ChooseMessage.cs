81d7e14244db314f60dcf2a615206efe19312061
martin_newbie <g.martin.min@gmail.com>
2022-07-25 23:14:27 +0900

    fix popup

--- Assets/Scripts/Shop/MessageBox/ChooseMessage.cs
+++ Assets/Scripts/Shop/MessageBox/ChooseMessage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using DG.Tweening;
 
 public class ChooseMessage : MonoBehaviour, IPopUp
 {
@@ -11,9 +12,14 @@ public class ChooseMessage : MonoBehaviour, IPopUp
     public Text MessageTxt;
 
     MessageBoxContainer manager;
+    RectTransform rect;
 
     public void Init(Action accept, Action refuse, string message, MessageBoxContainer _manager)
     {
+        if (rect == null) rect = GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(0, -1200f);
+        rect.DOAnchorPosY(0f, 0.5f).SetEase(Ease.OutBack);
+
         manager = _manager;
 
         ShopUIManager.Instance.AddPopup(this);
@@ -26,7 +32,7 @@ public class ChooseMessage : MonoBehaviour, IPopUp
         refuse += manager.ClosePopup;
         accept += manager.ClosePopup;
 
-        AcceptButton.onClick.AddListener(()=> accept?.Invoke());
+        AcceptButton.onClick.AddListener(() => accept?.Invoke());
         RefuseButton.onClick.AddListener(() => refuse?.Invoke());
     }
 
@@ -38,6 +44,9 @@ public class ChooseMessage : MonoBehaviour, IPopUp
 
     public void Close()
     {
-        gameObject.SetActive(false);
+        rect.DOAnchorPosY(-1200f, 0.5f).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
     }
 }
--- Assets/Scripts/Shop/MessageBox/ConfirmMessage.cs
+++ Assets/Scripts/Shop/MessageBox/ConfirmMessage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using DG.Tweening;
 
 public class ConfirmMessage : MonoBehaviour, IPopUp
 {
@@ -10,9 +11,14 @@ public class ConfirmMessage : MonoBehaviour, IPopUp
     public Text MessageTxt;
 
     MessageBoxContainer manager;
+    RectTransform rect;
 
     public void Init(Action confirm, string message, MessageBoxContainer _manager)
     {
+        if (rect == null) rect = GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(0f, -1200f);
+        rect.DOAnchorPosY(0f, 0.5f).SetEase(Ease.OutBack);
+
         manager = _manager;
         ShopUIManager.Instance.AddPopup(this);
         MessageTxt.text = message;
@@ -33,6 +39,9 @@ public class ConfirmMessage : MonoBehaviour, IPopUp
 
     public void Close()
     {
-        gameObject.SetActive(false);
+        rect.DOAnchorPosY(0f, 0.5f).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
     }
 }
