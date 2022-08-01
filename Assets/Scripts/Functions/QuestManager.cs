08cf804b4552c732f0a0352cd7396d4648118bc1
martin_newbie <g.martin.min@gmail.com>
2022-08-01 23:54:20 +0900

    add quest data and sample quest

--- Assets/Scripts/Functions/QuestManager.cs
+++ Assets/Scripts/Functions/QuestManager.cs
@@ -8,7 +8,27 @@ public class QuestManager : MonoBehaviour
 {
 }
 
-public class QuestData
+public enum RewardType
 {
+    COIN,
+    ENERGY
+}
 
+public abstract class QuestData
+{
+    public string questName;
+    public string questDesc;
+    public RewardType rewardType;
+    public int rewardValue;
+
+    public abstract float curProgress { get; }
+    public float maxProgress;
+    public bool isQuestClear => curProgress >= maxProgress;
+}
+
+
+// sample
+public class QuestSample : QuestData
+{
+    public override float curProgress => GameManager.Instance.bestScore;
 }
