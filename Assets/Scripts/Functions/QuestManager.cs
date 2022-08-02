07e6ec1487155ac21b102f1bcfbb4241848ec1c0
martin_newbie <g.martin.min@gmail.com>
2022-08-02 10:48:32 +0900

    add saving quest data

--- Assets/Scripts/Functions/QuestManager.cs
+++ Assets/Scripts/Functions/QuestManager.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 
 
-public class QuestManager : MonoBehaviour
+public class QuestManager : Singleton<QuestManager>
 {
+    public string questSaveName = "QuestData";
+    public List<QuestData> Quests = new List<QuestData>();
+
+    private void Awake()
+    {
+        string saveData = PlayerPrefs.GetString(questSaveName, "None");
+        if(saveData == "None")
+        {
+            // init quest
+        }
+        else
+        {
+            SetSaveData(saveData);
+        }
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause) PlayerPrefs.SetString(questSaveName, GetSaveData());
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString(questSaveName, GetSaveData());
+    }
+
+    string GetSaveData()
+    {
+        string saveData = JsonUtility.ToJson(Quests, true);
+        return saveData;
+    }
+
+    void SetSaveData(string saveData)
+    {
+        Quests = JsonUtility.FromJson<List<QuestData>>(saveData);
+    }
 }
 
 public enum RewardType
@@ -14,6 +50,7 @@ public enum RewardType
     ENERGY
 }
 
+[System.Serializable]
 public abstract class QuestData
 {
     public string questName;
@@ -28,6 +65,7 @@ public abstract class QuestData
 
 
 // sample
+[System.Serializable]
 public class QuestSample : QuestData
 {
     public override float curProgress => GameManager.Instance.bestScore;
