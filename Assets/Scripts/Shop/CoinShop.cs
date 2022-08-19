6967edda01c0c4f5b03cdda73b36d0fd8f2e505c
martin_newbie <g.martin.min@gmail.com>
2022-08-20 00:32:13 +0900

    dd

---
+++ Assets/Scripts/Shop/CoinShop.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LanguageType
+{
+    Kor,
+    Eng
+}
+
+public class CoinShop : MonoBehaviour
+{
+    public LanguageType language;
+    public string path => "Sprite/Shop/coin shop ui";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
