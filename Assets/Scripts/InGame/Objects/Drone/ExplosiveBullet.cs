084f869d1c8deb4f404e28650659bfd7371ce62e
martin-newbie <g.martin.min@gmail.com>
2022-08-27 20:15:33 +0900

    apply bullet resource

--- Assets/Scripts/InGame/Objects/Drone/ExplosiveBullet.cs
+++ Assets/Scripts/InGame/Objects/Drone/ExplosiveBullet.cs
@@ -7,6 +7,21 @@ public class ExplosiveBullet : Bullet
     [Header("Explosive Bullet")]
     public float radius;
     public GameObject explosion;
+    public ParticleSystem smokeEffect;
+
+    protected override void OnBecameInvisible()
+    {
+        RemoveSmoke();
+        base.OnBecameInvisible();
+    }
+
+    void RemoveSmoke()
+    {
+        smokeEffect.Stop();
+        smokeEffect.transform.SetParent(null);
+        smokeEffect.transform.localScale = Vector3.one;
+        Destroy(smokeEffect.gameObject, 2f);
+    }
 
     protected override void AttackAction(Collider2D collision)
     {
@@ -15,6 +30,8 @@ public class ExplosiveBullet : Bullet
         Instantiate(explosion, transform.position, Quaternion.identity);
 
         Collider2D[] objs = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask("Hostile"));
+        RemoveSmoke();
+        
         foreach (var item in objs)
         {
             Entity temp;
